ab0e0b690defa7e0f0e1e16ce99236139df851ce
Peter B Yorke <pbyorke@gmail.com>
2015-12-14 11:11:50 -0500

    redo the timing loop
    put the timer into an NSLoop that is not affected by the UI.
    set the timer for a fixed amount of time, then count ticks when it goes off
    make the slider taller

--- GLSearchFast Extension/WordsInterfaceController.swift
+++ GLSearchFast Extension/WordsInterfaceController.swift
@@ -9,7 +9,16 @@
 import WatchKit
 import Foundation
 
-
+/*
+ * class WordsInterfaceController
+ *
+ * Control the view that displays words at a measured pace. The word
+ * is displayed in the middle of the screen in a large font. A slider
+ * paired with a display of the number of words per minute gives the
+ * user control over the pace. Start and Stop buttons allow the display
+ * to be frozen and the timer stopped
+ *
+ */
 class WordsInterfaceController: WKInterfaceController {
     
     @IBOutlet var word: WKInterfaceLabel!
@@ -17,11 +26,47 @@ class WordsInterfaceController: WKInterfaceController {
     let words = Words()
     let delegate = WKExtension.sharedExtension().delegate as! ExtensionDelegate
     var interval = 100.00
+    var counter = 0.0
     
+    /*
+     * doStart()
+     * action performed when the Start button is pressed
+     */
     @IBAction func doStart() {
-        delegate.timer = NSTimer.scheduledTimerWithTimeInterval(60.0 / interval, target: self, selector: "showNextWord", userInfo: nil, repeats: true)
+        if let _ = delegate.timer { // make sure we have a timer
+            while delegate.timer.valid { // keep trying to stop it until it's not valid (stopped)
+                doStop()
+            }
+        }
+        delegate.timer = NSTimer(timeInterval: 0.1, // fire off a timer every 0.10 seconds
+            target: self,
+            selector: "timerFired",
+            userInfo: nil,
+            repeats: true)
+        NSRunLoop.currentRunLoop().addTimer(delegate.timer, forMode: NSRunLoopCommonModes) // put into loop that UI isn't using
     }
     
+    /*
+     * timerFired()
+     * the timer has fired, now we need to figure out if this is the 'right' firing for this wpm
+     *
+     * if we assume a max of 600 wpm, that works out to one every 0.10 second. To find out how
+     * many timer firings we need for a given wpm we divide that number into 600
+     */
+    func timerFired() {
+        counter++ // another tick of the timer
+        let threshold = 600.0 / interval // number of ticks needed for this wpm
+        if counter >= threshold { // okay, time to show a word
+            counter = 0.0 // reset
+            showNextWord()
+        }
+    }
+
+    /*
+     * showNextWord()
+     * get the next word to be shown, then dispatch the main
+     * queue and put the word into the UI
+     */
     func showNextWord() {
         dispatch_async(dispatch_get_main_queue(), {
             () -> Void in
@@ -29,21 +74,29 @@ class WordsInterfaceController: WKInterfaceController {
         })
     }
     
+    /*
+     * doStop()
+     * action perfored when the Stop button is pressed
+     *
+     * basically, stop the timer
+     */
     @IBAction func doStop() {
         if let _ = delegate.timer {
             delegate.timer.invalidate()
         }
     }
-    
-    func newInterval(newinterval: Double) {
-        doStop()
-        interval = newinterval
-        wpm.setText("\(newinterval) WPM")
-        doStart()
-    }
-    
-    @IBAction func doSlide(value: Float) {
-        newInterval(Double(value))
+
+    /*
+     * newInterval()
+     * receive notification from the slider that a new value has been
+     * chosen
+     *
+     * stuff the new interval in the common var where timerFired() will find
+     * it, and display the new vlaue on the screen
+     */
+    @IBAction func newInterval(value: Float) {
+        interval = Double(value)
+        wpm.setText("\(Int(value)) WPM")
     }
     
 }
